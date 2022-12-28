4f23d9481c64a217c39924d799eba6af29c6078f
Sebastian Daschner <git@sebastian-daschner.de>
2022-12-28 07:53:41 +0100

    updated to JDK 17; changed DTO to record class;

--- src/main/java/com/neo4j/examples/movies/quarkus/persons/entities/ActorRecommendation.java
+++ src/main/java/com/neo4j/examples/movies/quarkus/persons/entities/ActorRecommendation.java
@@ -1,11 +1,5 @@
 package com.neo4j.examples.movies.quarkus.persons.entities;
 
-/**
- * <strong>NOTE:</strong> As of Neo4j OGM Quarkus {@code 2.0.0}, DTO mapping classes must have a public, no-arg constructor.
- */
-public class ActorRecommendation {
-
-    public String actor;
-    public long strength;
+public record ActorRecommendation(String actor, long strength) {
 
 }
